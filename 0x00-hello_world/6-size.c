a007c46f10ebcd44a11989da3eec9e3d1476bf10
lephotombuyiselo <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A@github.com>
2023-02-16 16:24:41 -0800

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	float f;
 printf("Size of a  char: %lu btye(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu btye(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu btye(s)\n", (unsigned long)sizeof(f));
 return (0);
