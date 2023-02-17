f03ae28fc48097198e4db3a3dd5d489453cd7e68
lephotombuyiselo <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A@github.com>
2023-02-16 18:23:21 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
- * main - A program that determines sizes of data type
- * Return: 0 (Success)
+ * main - A program display variable type size
+ * Return 0 (Success)
  */
-
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
-printf("Size of a char: %lu btye(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu btye(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu btye(s)\n", (unsigned long)sizeof(f));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n",(unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
