0288be4cde62951eb8bf131b4d07e4efffc95628
lephotombuyiselo <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A@github.com>
2023-02-17 13:33:33 -0800

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  main  - print alphabet in lowercase.
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[26} = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
