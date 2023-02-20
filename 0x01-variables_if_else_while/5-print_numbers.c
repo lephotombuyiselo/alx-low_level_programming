11466d5775380d712a7b48072645223c2eb344d7
lephotombuyiselo <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A@github.com>
2023-02-20 04:29:09 -0800

    Print Number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-
 /**
-* main - print number 0 to 9.
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+ * main - A programm that print numbers from 0 to 9.
+ * Return: Always 0 (Succes)
+ */
+int  main(void)
 {
-	char i;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar(i);
+		printf("%d", i);
 	}
 	putchar('\n');
 	return (0);
